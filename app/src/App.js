e0f41f67ed5d53c2fec66b90c9ffc33a36d6a152
Rafiq Lightwala <rafiq.lightwala@gmail.com>
2024-01-15 15:50:16 -0700

    Added Fetch

--- app/src/App.js
+++ app/src/App.js
@@ -6,7 +6,43 @@ function App() {
   const [eventName, setEventName] = useState("");
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
-  const [eventCreated, setEventCreated] = useState(false);
+  const [eventData, setEventData] = useState(null);
+  const [eventErrorData, setEventErrorData] = useState(null);
+
+
+  const addEvent = async (eventName, startingDate, endingDate) => {
+
+      const response = await fetch('http://localhost:4000/v1/events', {
+        method: 'POST',
+        body: JSON.stringify({
+          name: eventName,
+          startDate: startingDate,
+          endDate: endingDate,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+  
+      //console.log('Response status code:', response.status); 
+      const returnedData = await response.json();
+
+      if (response.status !== 201) {
+        // Handle non-201 status
+        setEventErrorData(returnedData);
+        console.log(returnedData.message)
+        // Optionally, you can throw an error or return early
+        //throw new Error(`Request failed with status ${response.status}`);
+      } else {
+        setEventData(returnedData);
+        setEventName(""); // Reset eventName to an empty string
+        setStartDate(""); // Reset startDate to an empty string
+        setEndDate("");
+      }
+
+      //console.log(returnedData);
+  };
+  
 
   const handleCreateEventClick = () => {
     setShowForm(true);
@@ -16,10 +52,7 @@ function App() {
     e.preventDefault();
     // Perform event submission logic here (you can add more validation if needed)
     // For this example, we'll just set the event as created
-    setEventCreated(true);
-    setEventName(""); // Reset eventName to an empty string
-    setStartDate(""); // Reset startDate to an empty string
-    setEndDate("");
+    addEvent(eventName, startDate, endDate)
   };
 
   return (
@@ -61,7 +94,11 @@ function App() {
           <button type="submit">Submit</button>
         </form>
       )}
-      {eventCreated && <p>Thank you, Your Event has been Created</p>}
+
+      {eventData && <p> Your event with {eventData.eventDays} days has been created</p>}
+      {eventErrorData && <p>{eventErrorData.message}</p>}
+        
+
     </div>
   );
 }
