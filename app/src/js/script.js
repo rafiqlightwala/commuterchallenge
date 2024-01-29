87a6da08d51b4a859a2a192b66f210a1584ac974
alejandro2790 <alejandro.meixueiro@hotmail.com>
2024-01-29 14:06:54 -0700

    updated work

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,70 +1,13 @@
-import { getCommuterModes } from "./api.js";
-
 document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll('.slider-image');
+  let currentImageIndex = 0;
 
-  // PhotoSlider Component
-  const photoSlider = document.getElementById("photoSlider");
-  const photos = [
-    "img/photo1.jpg",
-    "img/photo2.jpg",
-    "img/photo3.jpg",
-    "img/photo4.jpg",
-    "img/photo5.jpg",
-    "img/photo6.jpg",
-    "img/photo7.jpg",
-    "img/photo8.jpg",
-    "img/photo10.jpg",
-    "img/photo11.jpg",
-    "img/photo13.jpg",
-  ];
-
-  // Function to preload images
-  const preloadImages = () => {
-    photos.forEach((src) => {
-      const img = new Image();
-      img.src = src;
-    });
-  };
-
-  let currentPhotoIndex = 0;
-
-  const updateSlider = () => {
-    photoSlider.innerHTML = `
-        <div class="slide">
-          <img src="${photos[currentPhotoIndex]}" alt="Photo ${
-      currentPhotoIndex + 1
-    }">
-          <div class="slide-overlay">
-            <p class="slide-text">
-              <span class="challenge-text">ARE YOU READY FOR THE NEXT CHALLENGE?</span><br>
-              <span class="date-text">June 2-8, 2024</span>
-            </p>
-            <button class="slide-button">REGISTER NOW!</button>
-          </div>
-        </div>
-      `;
-  };
-
-  preloadImages();
-  updateSlider();
-
-  setInterval(() => {
-    currentPhotoIndex = (currentPhotoIndex + 1) % photos.length;
-    updateSlider();
-  }, 3000);
-
-
-  //IGNORE THE FOLLOWING LINE IT IS JUST CHECKING CODE
-  fetchCommuterModes();
-});
-
-async function fetchCommuterModes() {
-  try {
-    const commuterModesArray = await getCommuterModes();
-    console.log(commuterModesArray);
-    // Now you can call setCommuterModesArray or any other logic you need
-    // setCommuterModesArray(commuterModesArray);
-  } catch (error) {
-    console.error("Error fetching commuter modes:", error);
+  function changeImage() {
+    images.forEach(img => img.classList.remove('active'));
+    images[currentImageIndex].classList.add('active');
+    currentImageIndex = (currentImageIndex + 1) % images.length;
   }
-}
+
+  setInterval(changeImage, 3000); // Change image every 3 seconds
+  changeImage(); // Initialize first image
+});
--- server/src/app.js
+++ server/src/app.js
@@ -1,34 +1,34 @@
-const express = require('express');
-const config = require('./config/config');
-const cors = require('cors')
-const httpStatus = require('http-status');
-const routes = require('./routes/v1');
-const { errorConverter, errorHandler } = require('./middlewares/error');
-const ApiError = require('./utils/ApiError');
+// const express = require('express');
+// const config = require('./config/config');
+// const cors = require('cors')
+// const httpStatus = require('http-status');
+// const routes = require('./routes/v1');
+// const { errorConverter, errorHandler } = require('./middlewares/error');
+// const ApiError = require('./utils/ApiError');
 
-const app = express();
+// const app = express();
 
-// parse json request body
-app.use(express.json());
+// // parse json request body
+// app.use(express.json());
 
-// parse urlencoded request body
-app.use(express.urlencoded({ extended: true }));
+// // parse urlencoded request body
+// app.use(express.urlencoded({ extended: true }));
 
-//use cors
-app.use(cors())
+// //use cors
+// app.use(cors())
 
-// v1 api routes
-app.use('/v1', routes);
+// // v1 api routes
+// app.use('/v1', routes);
 
-// send back a 404 error for any unknown api request
-app.use((req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
-});
+// // send back a 404 error for any unknown api request
+// app.use((req, res, next) => {
+//   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+// });
 
-// convert error to ApiError, if needed
-app.use(errorConverter);
+// // convert error to ApiError, if needed
+// app.use(errorConverter);
 
-// handle error
-app.use(errorHandler);
+// // handle error
+// app.use(errorHandler);
 
-module.exports = app;
+// module.exports = app;
